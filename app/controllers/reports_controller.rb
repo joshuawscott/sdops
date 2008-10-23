eb1af33b48363a147a11d08a9cc0d221a67a4092
Troy Nini <tnini@dev-jeos.(none)>
2008-10-23 12:22:34 -0500

    Get 'Filter by Office' working on renewal report

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -37,6 +37,10 @@ class ReportsController < ApplicationController
 
     
     @contracts = Contract.renewals_next_90_days(current_user.role, current_user.sugar_team_ids, @ref_date)
+    @offices = @contracts.map{|x| x.sales_office_name}
+    @offices.uniq!.sort!
+    
+
     respond_to do |format|
       format.html # renewals.html.haml
     end
