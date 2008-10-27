6c418bab951287865de5d4530a0b1a99f80f0005
Troy Nini <tnini@dev-jeos.(none)>
2008-10-27 10:35:01 -0500

    Fix javascript to work with IE7

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,38 @@
-// Place your application-specific JavaScript functions and classes here
-// This file is automatically included by javascript_include_tag :defaults
+
+// ************************************************
+// Functions for Renewals report
+// ************************************************
+function toggleOffices(){
+  var office = $('filter_offices').value;
+  var a = $('filter_offices').options;
+  if (office == ""){
+    showAll();
+  }
+  else {
+    showAll();
+    for (i=0; i < a.length; i++){
+      if (a[i].value != office && a[i].value !=""){
+        var name = 'tr[id="'+a[i].value+'"]';
+        $$(name).invoke('hide');
+      }
+    }
+  }
+}
+
+function showAll(){
+  var a = $('filter_offices').options;
+  for (i=0; i < a.length; i++){
+    var name = 'tr[id="'+a[i].value+'"]';
+    $$(name).invoke('show');
+  }
+}
+
+
+// ************************************************
+// Functions for Dashboard report
+// ************************************************
+function toggleHeaderStuff(){
+  $('header').toggle();
+  $('submenu').toggle();
+}
+
