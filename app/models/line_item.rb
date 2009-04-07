51f92fd37a1a23f6f8e1c897ea3dac74b31b7b83
Joshua Scott <jscott@sourcedirect.com>
2009-04-07 14:51:49 -0500

    fixed bug in LineItem self.locations method

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -2,7 +2,7 @@ class LineItem < ActiveRecord::Base
   belongs_to :contract
   def self.locations(role,teams)
     if role >= MANAGER
-      LineItem.find(:all, :select => 'location', :conditions => ['contract.expired = ?', false]).map {|x| x.location}.uniq!.sort!
+      LineItem.find(:all, :select => 'location', :joins => :contract, :conditions => ['contracts.expired = ?', false]).map {|x| x.location}.uniq!.sort!
     else
       SugarTeam.dropdown_list(role,teams).map {|x| x.name}
     end
