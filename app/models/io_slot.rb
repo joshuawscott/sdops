e6de3ec17e1dcda4112ed98caf07dc2355408870
Joshua Scott <jscott@sourcedirect.com>
2009-04-29 16:55:38 -0500

    Added validation to IoSlot model to prevent nil errors

--- app/models/io_slot.rb
+++ app/models/io_slot.rb
@@ -9,7 +9,7 @@
 class IoSlot < ActiveRecord::Base
   belongs_to :server
   validates_uniqueness_of :path, :scope => [:server_id]
-
+  validates_presence_of :io_slot
   def sort_order
     ((chassis_number.nil? ? 0 : chassis_number) * 1000) + slot_number
   end
