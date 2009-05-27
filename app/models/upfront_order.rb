b18f125a651db35cb20a0b085d0b84e8029ebe57
Joshua Scott <jscott@sourcedirect.com>
2009-05-27 14:13:39 -0500

    Added callback to update completed when a contract is deleted.

--- app/models/upfront_order.rb
+++ app/models/upfront_order.rb
@@ -6,10 +6,18 @@ class UpfrontOrder < ActiveRecord::Base
   belongs_to :appgen_order
   belongs_to :contract
   validates_uniqueness_of :appgen_order_id
+  after_save :update_completed
+  
   def self.update_from_appgen
     AppgenOrder.find(:all, :joins => "LEFT JOIN upfront_orders ON appgen_orders.id = upfront_orders.appgen_order_id", :conditions => "upfront_orders.id IS NULL").each do |appgen_order|
       @upfront_order = UpfrontOrder.new( :appgen_order_id => appgen_order.id )
       @upfront_order.save
     end
   end
+
+  protected
+  def update_completed
+    update_attribute(:completed, false) if self.contract_id == nil && self.completed == true
+  end
+  
 end
