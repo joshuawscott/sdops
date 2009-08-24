fe760cde8bdd2848efe9ddb68b899a6389e3be1e
root <root@dev-jeos.sourcedirect.com>
2009-08-24 15:54:14 -0500

    Added ternary operator to ensure it puts one for field count
    Before if the multivalued field was blank it was messing up the counts and hence dropping records.

--- app/models/appgen.rb
+++ app/models/appgen.rb
@@ -77,7 +77,9 @@ class Appgen
       metarecord.each_with_index do |metarec,row|
         record = metarec[0].split("\n")
         record.each_with_index {|r,idx| record[idx] = r.split(":")[1].to_s.strip}
-        recs_to_create = mv_field_count > 0 ? (record.size - reg_field_count) / mv_field_count : 1
+        #Ensure there is allways 1 for the field counts
+        field_count = (record.size - reg_field_count) / mv_field_count <= 0 ? 1 : (record.size - reg_field_count) / mv_field_count
+        recs_to_create = mv_field_count > 0 ? field_count : 1
         recs_to_create.times {ary << []}
         recs_to_create.times do |z|
           ary[i][0..reg_field_count - 1] = record[0..reg_field_count - 1]
