16f9811ef1a68d2bd6bc62b164fb7934322aa682
Joshua Scott <jscott@sourcedirect.com>
2010-12-10 23:41:18 -0600

    Troy's Latte ;)
    Introduced javascript to sum the expired contracts in the renewals report.
    The sum changes based on the office chosen.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -9,7 +9,7 @@ function toggleOffices(){
   }
   else {
     for (i=0; i < a.length; i++){
-      var name = 'tr[id="'+a[i].value+'"]';
+      var name = 'tr[name="'+a[i].value+'"]';
       if (a[i].value != office && a[i].value !=""){
         $$(name).invoke('hide');
       }
@@ -18,14 +18,30 @@ function toggleOffices(){
       }
     }
   }
+  sumVisibleRows();
 }
 
 function showAll(){
   var a = $('filter_offices').options;
   for (i=0; i < a.length; i++){
-    var name = 'tr[id="'+a[i].value+'"]';
+    var name = 'tr[name="'+a[i].value+'"]';
     $$(name).invoke('show');
   }
+  sumVisibleRows();
+}
+
+function sumVisibleRows(){
+	var rows = $$('#expired_table tbody tr');
+	var sum = 0.0;
+	rows.each(function(r) {
+			if (r.getAttribute("style") != 'display: none;') {
+				var price = r.cells[7].innerHTML.replace(/[\$\,]/g, '');
+				sum += (price * 1.0);
+				//console.log(r.getAttribute("name"));
+			}
+	});
+	console.log(sum);
+	$('expired_total').innerHTML = number_to_currency(sum);
 }
 
 // Print view
@@ -43,3 +59,15 @@ function commandDescOff(id){
   $(id).innerHTML = '&nbsp;';
 }
 
+// Basic functions
+
+function number_to_currency(Amount) {
+	var AmountWithCommas = Amount.toLocaleString();
+	var arParts = String(AmountWithCommas).split('.');
+	var intPart = arParts[0];
+	var decPart = (arParts.length > 1 ? arParts[1] : '');
+	decPart = (decPart + '00').substr(0,2);
+
+	return '$' + intPart + '.' + decPart;
+
+}
