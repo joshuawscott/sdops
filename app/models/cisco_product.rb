dfca0cd8114473a2cfd6f3d95e9d333c33d61660
Joshua Scott <jscott@sourcedirect.com>
2011-12-20 11:51:16 -0600

    partial rewrite of the cisco pricing to account for changes to the source website.

--- app/models/cisco_product.rb
+++ app/models/cisco_product.rb
@@ -9,10 +9,11 @@ class CiscoProduct
   def initialize(pn)
     @part_number = pn
 
-    @price_xpath = "/html/body[@class='clsmargin']/table/tr[3]/td/table/tr/td/table/tr/td[2]/table/tr[3]/td/table/tr[5]/td[2]/table/tr[1]/td[4]/table[@class='tw']/tr[4]/td/table/tr/td/font/strong"
+    #@price_xpath = "/html/body[@class='clsmargin']/table/tr[3]/td/table/tr/td/table/tr/td[2]/table/tr[3]/td/table/tr[5]/td[2]/table/tr[1]/td[4]/table[@class='tw']/tr[4]/td/table/tr/td/font/strong"
+    @price_xpath = "//label[@id='calprice']/a"
     @description_url = "http://www.ithsc.com/ciscohardwaremaintenance/SMARTnet_calculator.php"
-    @description_xpath = "/html/body[@class='clsmargin']/table/tr[3]/td[1]/table/tr/td/table/tr/td[2]/table/tr[3]/td/table/tr[5]/td/table[@class='infoBox']/tr[@class='infoBoxContents']/td/table[2]/table/tr[4]/td[2]"
-
+    #@description_xpath = "/html/body[@class='clsmargin']/table/tr[3]/td[1]/table/tr/td/table/tr/td[2]/table/tr[3]/td/table/tr[5]/td/table[@class='infoBox']/tr[@class='infoBoxContents']/td/table[2]/table/tr[4]/td[2]"
+    @description_xpath = "//div[@id='qbcontent']/div[@class='listitem']/div[@class='desc']"
   end
 
   # This is a constructor method that prevents going out to the external site.
@@ -35,16 +36,16 @@ class CiscoProduct
   end
 
   def hw_price
-    ((full_price - base_price) / 0.8) / 12.0
+    ((full_price - base_price) / 0.85) / 12.0
   end
 
   def sw_price
-    (base_price / 0.8) / 12.0
+    (base_price / 0.85) / 12.0
   end
 
   def description
     return @description unless @description.nil?
-    @description = Nokogiri::HTML(Net::HTTP.post_form(URI.parse(description_url), {'product' => URI.escape(part_number), 'Submit' => 'Find+Cisco+Product+to+provide+cover+for..'}).body).xpath(@description_xpath)
+    @description = Nokogiri::HTML(Net::HTTP.post_form(URI.parse(description_url), {'product' => URI.escape(part_number), 'submit1' => ''}).body).xpath(@description_xpath)
     if @description[0].nil?
       @description = ""
     else
@@ -59,6 +60,7 @@ class CiscoProduct
   def base_price
     return @base_price unless @base_price.nil?
     @base_price = Nokogiri::HTML(open(base_url)).xpath(@price_xpath)
+    puts "base price: #{@base_price}"
     if @base_price[0].nil?
       @base_price = 0.0
     else
@@ -73,6 +75,7 @@ class CiscoProduct
   def full_price
     return @full_price unless @full_price.nil?
     @full_price = Nokogiri::HTML(open(full_url)).xpath(@price_xpath)
+    puts "full price: #{@full_price}"
     if @full_price[0].nil?
       @full_price = 0.0
     else
--- spec/models/cisco_product_spec.rb
+++ spec/models/cisco_product_spec.rb
@@ -4,20 +4,20 @@ describe CiscoProduct do
 
   before(:all) do
     @cisco_product = CiscoProduct.new('asdf')
-    @cisco_product.stub!(:base_price).and_return 300.00
-    @cisco_product.stub!(:full_price).and_return 600.00
+    @cisco_product.stub!(:base_price).and_return 1020.00
+    @cisco_product.stub!(:full_price).and_return 2040.00
     @cisco_product.stub!(:description).and_return "This is a test"
   end
 
   describe "hw_price" do
     it "should calculate the hw_price" do
-      @cisco_product.hw_price.should == 31.25
+      @cisco_product.hw_price.should == 100
     end
   end
 
   describe "sw_price" do
     it "should calculate the sw_price" do
-      @cisco_product.sw_price.should == 31.25
+      @cisco_product.sw_price.should == 100
     end
   end
 
