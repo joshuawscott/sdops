8e63d64aca1d3f82245e530305ddf6f09fda5eb4
Joshua Scott <jscott@sourcedirect.com>
2012-03-30 09:17:18 -0500

    fix SAM report (was ==1, needed == true)

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -139,6 +139,6 @@ class ReportsController < ApplicationController
 
   def sam_needed
     @customers = Contract.find(:all, :select => 'support_deals.*, min(start_date) as min_start_date ', :conditions => '(expired = 0) AND (basic_remote_monitoring = 1 OR basic_backup_auditing = 1)', :group => 'account_id')
-    @customers.delete_if {|customer| customer.sugar_acct.sugar_accounts_cstm.rmmhubdeployed_c == 1}
+    @customers.delete_if {|customer| customer.sugar_acct.sugar_accounts_cstm.rmmhubdeployed_c == true}
   end
 end
