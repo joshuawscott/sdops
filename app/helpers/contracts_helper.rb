e677f34c7f93a4a1982635b93c78c60818be72ab
Joshua Scott <jscott@sourcedirect.com>
2012-11-12 08:47:33 -0600

    Bugfix for Bug 55

--- app/helpers/contracts_helper.rb
+++ app/helpers/contracts_helper.rb
@@ -6,7 +6,11 @@ module ContractsHelper
     if line_item.subcontract_id.nil?
       haml_concat line_item.support_provider
     else
-      haml_concat(link_to(line_item.subcontract.subcontractor.name, contract_line_item_path(@contract, line_item)))
+      if line_item.subcontract.nil?
+        haml_concat "ERROR"
+      else
+        haml_concat(link_to(line_item.subcontract.subcontractor.name, contract_line_item_path(@contract, line_item)))
+      end
     end
   end
 
