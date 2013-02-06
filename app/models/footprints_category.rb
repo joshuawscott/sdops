5fc4edd8a1e6e036011d86875e0bb34c2c2640ff
Joshua Scott <jscott@sourcedirect.com>
2013-02-06 08:09:36 -0600

    Fix bug in Model

--- app/models/footprints_category.rb
+++ app/models/footprints_category.rb
@@ -1,4 +1,4 @@
 class FootprintsCategory < ActiveRecord::Base
-  requires_presence_of :subsystem
-  requires_presence_of :main_category
+  validates_presence_of :subsystem
+  validates_presence_of :main_category
 end
